9d51b5d05769c0395aeec4c51576d85436c92508
Johan Haals <johan.haals@gmail.com>
2011-09-06 18:17:01 +0200

    login support

--- server/ppUpload_server.py
+++ server/ppUpload_server.py
@@ -12,6 +12,7 @@ from werkzeug import secure_filename
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
+from password import Password
 
 config = configparser.RawConfigParser()
 if not config.read([os.path.expanduser('~/.ppupload.conf') or 'ppupload.conf', '/etc/ppupload.conf']):
@@ -24,30 +25,29 @@ app.config['DATABASE'] = config.get('settings', 'database_path')
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
         file = request.files['file']
-        
         username = request.form['username']
         password = request.form['password']
-
-        # Validate username and password hash
-        # pasword (hash + salt)
-        if re.search('[^A-Za-z0-9_]', username):
-            return 'Illigal username'
-        if re.search('[^A-Za-z0-9_]', password):
-            return 'Illigal password'
-
+        download_password = request.form['download_password']
+        expire = '0'
+        one_time_download = '0'
+        
         # connect to sqlite and check if user exists
         conn = sqlite3.connect(app.config['DATABASE'])
         c = conn.cursor()
-        # We should add some salt here.
-        c.execute("select user_id from users where username='%s' and password='%s'" % (username, password))
-        result = c.fetchone()
+        c.execute("select id, password from users where username='%s'" % (username))
+        
+        try:
+            user_id, password_hash = c.fetchone()
+        except TypeError:        
+            return 'Error: Check username/password'
+        
+        pw = Password(config.get('settings', 'secret_key'))
 
-        if result == None:
-            return 'Unknown user'
-
-        userid = result[0]
-
+        if not pw.validate(password_hash,password):
+            return 'Invalid username/password'
+        
         allowed_expires = {
             '1h': datetime.now() + relativedelta(hours=1),
             '1d': datetime.now() + relativedelta(days=1),
@@ -57,12 +57,12 @@ def upload_file():
 
         if request.form['expire'] in allowed_expires:
             expire = allowed_expires[request.form['expire']].strftime('%Y%m%d%H%M')
-        else:
-            expire = '0'
-
-        one_time_download = '0'
+            
         if request.form['one_time_download'] == '1':
             one_time_download = '1'
+
+        if download_password:
+            download_password = pw.generate(download_password)
 
         # Generate download hash
         filename = secure_filename(request.form['filename'])
@@ -73,11 +73,12 @@ def upload_file():
             os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'], download_hash))
         except IOError:
             return 'Could not upload file'
+
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], download_hash, filename))
 
         # save info to database
-        c.execute("""insert into files (hash, user_id, filename, expire, one_time_download)
-          values ('%s','%s','%s','%s', '%s')""" % (download_hash, userid, filename, expire, one_time_download)) 
+        c.execute("""insert into files (hash, user_id, filename, expire, one_time_download, download_password)
+          values ('%s','%s','%s','%s', '%s', '%s')""" % (download_hash, user_id, filename, expire, one_time_download, download_password)) 
         conn.commit()
         c.close()
 
