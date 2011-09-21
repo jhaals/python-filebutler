5c86e95f3f908c1fbf4fbb9d5d2d5382f4b27e93
Johan Haals <johan.haals@gmail.com>
2011-09-21 19:49:44 +0200

    rewritten to a class

--- filevalidity.py
+++ filevalidity.py
@@ -3,32 +3,47 @@
 # Standard Library
 import os
 import sqlite3
-import ConfigParser as configparser
 from datetime import datetime
 
 # Third party
 from dateutil.relativedelta import relativedelta
 
-config = configparser.RawConfigParser()
-if not config.read([os.path.expanduser('~/.ppupload.conf') or 'ppupload.conf', '/etc/ppupload.conf']):
-    sys.exit("Couldn't read configuration file")
+class FileValidity:
+    def __init__(self, database_path, storage_path):
+        ''' connect to database '''
+        self.database_path = database_path
+        self.storage_path = storage_path
+        self.conn = sqlite3.connect(database_path)
+        self.c = self.conn.cursor()
 
-# connect to sqlite and check if file exists
-conn = sqlite3.connect(config.get('settings', 'database_path'))
-c = conn.cursor()
-c.execute("select hash, expire, one_time_download, filename, download_password from files where expire !=0")
+    def expired(expire, expire_date):
+        expire_date = datetime.strptime(expire_date, '%Y%m%d%H%M%S')
 
-for entry in c.fetchall():
-    download_hash, expire, one_time_download, filename, download_password = entry
+        if datetime.now() > expire_date:
+            # Expired
+            return True
+        else:
+            return False
 
-    expire = datetime.strptime(expire, '%Y%m%d%H%M%S')
-    if datetime.now() > expire:
+    def remove_expired_files(self):
+        ''' remove all expired files from database '''
+
+        self.c.execute("select hash, expire, one_time_download, filename, download_password from files where expire !=0")
+        for entry in self.c.fetchall():
+            download_hash, expire_date, one_time_download, filename, download_password = entry
+
+            if self.expired(expire_date):
+                print 'removed %s' % filename
+                self.remove_data(download_hash, filename)
+
+    def remove_data(self, download_hash, filename):
+        ''' remove file from storage and database '''
+
         try:
-            os.remove(os.path.join(config.get('settings', 'storage_path'), download_hash, filename))
-            os.removedirs(os.path.join(config.get('settings', 'storage_path'), download_hash))
+            os.remove(os.path.join(self.storage_path, download_hash, filename))
+            os.removedirs(os.path.join(self.storage_path, download_hash))
         except OSError as e:
-            print 'Failed to remove files %s' % e
-
-        c.execute("delete from files where hash=?", (download_hash,))
-        conn.commit()
-        c.close()
+            print 'Failed to remove file %s' % e
+        
+        self.c.execute("delete from files where hash=?", (download_hash,))
+        self.conn.commit()
