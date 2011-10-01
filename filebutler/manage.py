4bf76edab62e420e396537c1469f40618aad3994
Johan Haals <johan.haals@gmail.com>
2011-10-01 22:10:59 +0200

    ability to create/delete user and also change user password.

--- filebutler/manage.py
+++ filebutler/manage.py
@@ -2,14 +2,32 @@
 # Copyright (c) 2011, Johan Haals <johan.haals@gmail.com>
 # All rights reserved.
 
+# Standard library
 import os
 import sqlite3
 import ConfigParser as configparser
+import getpass
+from optparse import OptionParser
+
+# Local
 from password import Password
+from filevalidity import FileValidity
 
 config = configparser.RawConfigParser()
 if not config.read([os.path.expanduser('~/.ppupload.conf') or 'ppupload.conf', '/etc/ppupload.conf']):
     sys.exit("Couldn't read configuration file")
+
+usage = 'usage: %prog -h'
+parser = OptionParser(usage)
+parser.add_option('--user', '-u', help='Username', default=False)
+parser.add_option('--adduser', '-a', help='Add new user', action='store_true', default=False)
+parser.add_option('--deleteuser', '-d', help='One time download', action='store_true', default=False)
+parser.add_option('--changepassword', help='change user password', action='store_true', default=False)
+parser.add_option('--delete-expired-data', help='One time download', default=False, action='store_true')
+
+options, args = parser.parse_args()
+
+
 conn = sqlite3.connect(config.get('settings', 'database_path'))
 c = conn.cursor()
 
@@ -39,28 +57,49 @@ class User:
         if not self.exists(user):
             c.execute("""insert into users (username, password) values (?, ?)""", (user, self.pw.generate(password),))
             conn.commit()
-            return 'Done'
+            return 'Added %s' % user
         else:
-            return 'User already exists'
-        
+            return 'User %s already exists' % user
+
     def delete(self, user):
         ''' Delete user from database '''
         if self.exists(user):
              c.execute("""delete from users where username=?""", (user,))
              conn.commit()
-             return 'Done'
+             return 'Deleted %s' % user
         else:
-            return 'User does not exist'
+            return 'User %s does not exist' % user
 
     def change_password(self, user, password):
-        pass
+        if self.exists(user):
+            c.execute("""UPDATE users SET password=? WHERE username=?""", (self.pw.generate(password), user,))
+            conn.commit()
+            return 'Password successfully changed'
+        else:
+            return 'User %s does not exist' % user
 
 user = User(config.get('settings', 'secret_key'))
 
-# Create user foobar with password 123
-#print user.create('foobar', '123')
+if options.adduser:
+    if options.user:
+        password = getpass.getpass('Password: ')
+        print user.create(options.user, password)
+    else:
+        parser.error('Please specify username')
 
-# Delete user foobar
-#user.delete('foobar')
+if options.deleteuser:
+    if options.user:
+        print user.delete(options.user)
+    else:
+        parser.error('Please specify username')
 
+if options.changepassword:
+    if options.user:
+        password = getpass.getpass('New password: ')
+        print user.change_password(options.user, password)
+    else:
+        parser.error('Please specify username')
+if options.delete_expired_data:
+    #fw = FileValidity
+    print 'yes'
 c.close()
